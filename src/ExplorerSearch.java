6e979736a90e904e47d49d031862ef5d1c7dd784
alstondsouza1 <alstondsouza35@gmail.com>
2025-04-05 21:34:00 -0700

    started with a helper method

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,6 +32,28 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
-        return -1;
+
+        int[][] start = startingLocation(island);
+        boolean[][] visited = new boolean[island.length][island[0].length];
+        return reachableAreaHelper(island, start, visited);
+    }
+
+    public static int reachableAreaHelper(int[][] island, int[][] start, boolean[][] visited) {
+
+        int row = current[0];
+        int col = current[1];
+
+        if (row < 0 || row >= island.length || col < 0 || col >= island[0].length) {
+            return 0;
+        }
+
+        if (visited[row][col]) {
+            return 0;
+        }
+
+        if (island[row][col] == 2 || island[row][col] == 3) {
+            return 0;
+        }
+
     }
 }
