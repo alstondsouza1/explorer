bc407332457c01e33696a038b808e96dab643ae8
alstondsouza1 <alstondsouza35@gmail.com>
2025-04-07 12:36:54 -0700

    added some JUnit tests for method reachableArea

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -17,4 +17,49 @@ public class ExplorerSearchTest {
 
     // Add more tests here!
     // Come up with varied cases
+
+    @Test
+    public void testReachableArea_noStartPoint() {
+        int[][] island = {
+            {1, 1, 1},
+            {2, 3, 1},
+            {1, 1, 1}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(0, actual); 
+    }
+
+    @Test
+    public void testReachableArea_startSurrounding() {
+        int[][] island = {
+            {2, 2, 2},
+            {2, 0, 3},
+            {3, 2, 2}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual); 
+    }
+
+    @Test
+    public void testReachableArea_notAccessible() {
+        int[][] island = {
+            {3, 3, 3},
+            {3, 3, 3},
+            {3, 0, 3}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual); 
+    }
+
+    @Test
+    public void testReachableArea_allAccessible() {
+        int[][] island = {
+            {1, 1, 1},
+            {1, 1, 1},
+            {1, 0, 1}
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(9, actual); 
+    }
+
 }
